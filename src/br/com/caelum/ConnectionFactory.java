411bfc88f4f135f8729141a1434cd6ed4f738c41
Silas dos Santos Caimi <silas.caimi@cnpq.br>
2021-02-22 20:37:44 -0300

    fix Connection Factory - register driverManager

--- src/br/com/caelum/ConnectionFactory.java
+++ src/br/com/caelum/ConnectionFactory.java
@@ -8,6 +8,7 @@ public class ConnectionFactory {
 
 	public Connection getConnection() {
 		try {
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
 			return DriverManager.getConnection("jdbc:mysql://localhost/fj21?serverTimezone=UTC", "root", "admin");
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
